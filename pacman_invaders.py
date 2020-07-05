fc5ba83550fe124ea5272ed4420e73e2c0890dcc
Marcus Mors <alivezeh@gmail.com>
2020-07-05 11:34:54 -0500

    v0.0.2 agregamos un fantasma drogrado y el cursor del space invaders no cruza la barrera

--- pacman_invaders.py
+++ pacman_invaders.py
@@ -14,6 +14,7 @@ pygame.display.set_caption('Pacman invader')
 background = (0,0,0)
 on = True
 pacx,pacy = size[0]//4 - (pac.get_size()[0]//2) , size[1]//2 - (pac.get_size()[1]//2)
+ghostx,ghosty = size[0]//4 - (ghost.get_size()[0]//2), size[1] - (pac.get_size()[1])
 colour_mid = (255,255,255)
 width_mid = 3//2
 
@@ -32,11 +33,16 @@ def pos_ghost(x,y):
 
 nav = pygame.image.load('data/nave2.png')
 navx,navy = size[0]//4 + size[0]//2 -(nav.get_size()[0]//2) , size[1] - 2*(nav.get_size()[1])
+#ghost
+ghost_cont = 0
+ghost_cont_limit = random.randint(5,10)
+ghost_mov = random.randint(0,1)
+#ghost
 
 while on:
     screen.fill(background)
     pygame.draw.line(screen,colour_mid,[(horizontal//2),0],[(horizontal//2),vertical],width_mid)
-    
+    #fantasma
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             on = False
@@ -56,9 +62,6 @@ while on:
                 pacman_cambioy=0
             
     #pygame.key.set_repeat() #delay,intervalo
-    
-    pacx+=pacman_cambiox
-    pacy+=pacman_cambioy
     if pacx >= size[0]//2 - pac.get_size()[0]:
         pacx = size[0]//2 - pac.get_size()[0]
     elif pacx <=0:
@@ -68,8 +71,42 @@ while on:
     elif pacy <=0:
         pacy = 0
     
-    pos_nav(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
-    pos_pac(pacx,pacy)
+    if ghost_mov == 0 and ghost_cont <= ghost_cont_limit:
+        ghost_cambiox = random.randint(-1,1)
+        ghost_cont += 1
+    elif ghost_mov == 1 and ghost_cont <= ghost_cont_limit:
+        ghost_cambioy = random.randint(-1,1)
+        ghost_cont += 1
+    else:
+        ghost_cont = 0
+        ghost_cont_limit = random.randint(5,10)
+        ghost_mov = random.randint(0,1)
+    pacx +=pacman_cambiox
+    pacy +=pacman_cambioy
     
+    ghostx+=ghost_cambiox
+    ghosty+=ghost_cambioy
+    if ghostx >= size[0]//2 - ghost.get_size()[0]:
+        ghostx = size[0]//2 - ghost.get_size()[0]
+    elif ghostx <=0:
+        ghostx=0 
+    if ghosty >= size[1]- ghost.get_size()[0]:
+        ghosty = size[1] - ghost.get_size()[0]
+    elif ghosty <=0:
+        ghosty = 0
+
+    navx,navy = pygame.mouse.get_pos()[0]-nav.get_size()[0]//2,pygame.mouse.get_pos()[1]-nav.get_size()[0]//2
+    if navx >= size[0] - nav.get_size()[0]:
+        navx = size[0] - nav.get_size()[0]
+    elif navx <=size[0]//2: 
+        navx=size[0]//2
+    if navy >= size[1]- nav.get_size()[1]:
+        navy = size[1] - nav.get_size()[1]
+    elif navy <=0:
+        navy = 0
+    
+    pos_nav(navx,navy)
+    pos_pac(pacx,pacy)
+    pos_ghost(ghostx,ghosty)
     pygame.mouse.set_visible(False)
     pygame.display.update()
