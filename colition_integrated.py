b70ecb6bb37889b046fffad52e978aeed22eebf7
Marcus Mors <alivezeh@gmail.com>
2020-07-06 23:39:59 -0500

    pre-integration (main menu update and colision feature) making functions to make it easier, main menu isn't encapulated yet

--- colition_integrated.py
+++ colition_integrated.py
@@ -2,142 +2,186 @@ import pygame
 import random
 import math
 from pygame import mixer
-
-def init():
-    global screen,size, horizontal, vertical, pac, bala, ghost, background, white, icon, width_mid, tile, black, colour_b1
-    pygame.init()
-    size = horizontal , vertical= 16*64, 33*16     
-    screen = pygame.display.set_mode((size))
-    tile = pygame.image.load("data/white_block.png")
-    mixer.music.load("Sonidos/snd_title_song.ogg")
-    mixer.music.play(-1)
+import os
+import sys
+def image_music_display():
+    global pac, ghost, bala
     pac = pygame.image.load('data/basic_pacman.png')
     ghost = pygame.image.load('data/basic_fantasma.png')
     bala = pygame.image.load('data/bala.png')
     icon = pac
     pygame.display.set_icon(icon)
     pygame.display.set_caption('Pacman invader')
-    
-    background = (180,20,60)
+    mixer.music.load("Sonidos/snd_title_song.ogg")
+    mixer.music.play(-1)
+    os.environ["SDL_ViDEO_CENTERED"] = "1"
+
+def colours():
+    global col_bg, white, col_map
+    col_bg = (180,20,60)
     white = (255,255,255)
-    black = (0,0,0)
-    colour_b1 = (40, 95, 141)
-    colour_map = (0,0,200)
+    col_map = (0,0,200)
+
+def colision():
+    global Player, player, clock, walls, end_rect
+    class Player(object):
+        def __init__(self):
+            self.rect = pygame.Rect(32, 32, 16, 16)
+            #self.pacman = pygame.
+        def move(self, dx, dy):
+            # Move each axis separately. Note that this checks for collisions both times.
+            if dx != 0:
+                self.move_single_axis(dx, 0)
+            if dy != 0:
+                self.move_single_axis(0, dy)    
+        def move_single_axis(self, dx, dy):
+            self.rect.x += dx
+            self.rect.y += dy
+            for wall in walls:
+                if self.rect.colliderect(wall.rect):
+                    if dx > 0: # Moving right; Hit the left side of the wall
+                        self.rect.right = wall.rect.left
+                    if dx < 0: # Moving left; Hit the right side of the wall
+                        self.rect.left = wall.rect.right
+                    if dy > 0: # Moving down; Hit the top side of the wall
+                        self.rect.bottom = wall.rect.top
+                    if dy < 0: # Moving up; Hit the bottom side of the wall
+                        self.rect.top = wall.rect.bottom
+    class Wall(object):
+        def __init__(self, pos):
+            walls.append(self)
+            self.rect = pygame.Rect(pos[0], pos[1], b_p, b_p)
+    clock = pygame.time.Clock()
+    walls = [] # List to hold the walls
+    player = Player() # Create the player
+    level =[
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'X           XX    XX           X',
+    'X XXXX XXXX XX XX XX XXXX XXXX X',
+    'X XXXX XXXX XX XX XX XXXX XXXX X',
+    'X XX           XX           XX X',
+    'X XX XXXXXX XX XX XX XXXXXX XX X',
+    'X    X   XX XX XX XX XX   X    X',
+    'X XX X      XX    XX      X XX X',
+    'X XX X   XXXXX    XXXXX   X XX X',
+    'X XX XXXXXXXXX    XXXXXXXXX XX X',
+    'X              XX              X',
+    'X XXXX XXXXXXX XX XXXXXXX XXXX X',
+    'X XXXX XXXXXXX XX XXXXXXX XXXX X',
+    'X      XXX            XXX      X',
+    'XXXXXX XXX XXXX  XXXX XXX XXXXXX',
+    'XXXXXX XXX X        X XXX XXXXXX',
+    '           X        X           ',
+    'XXXXXX XXX X        X XXX XXXXXX',
+    'XXXXXX XXX XXXXXXXXXX XXX XXXXXX',
+    'X      XXX            XXX      X',
+    'X XXXX XXX XXXXXXXXXX XXX XXXX X',
+    'X    X XXX XXXXXXXXXX XXX X    X',
+    'X    X         XX         X    X',
+    'X    X XXXXXXX XX XXXXXXX X    X',
+    'X XXXX XXXXXXX XX XXXXXXX XXXX X',
+    'X          XXX    XXX          X',
+    'X XXXXXXXX XXX XX XXX XXXXXXXX X',
+    'X XXXXXXXX XXX XX XXX XXXXXXXX X',
+    'X      XXX     XX     XXX      X',
+    'X XXXX XXX XXXXXXXXXX XXX XXXX X',
+    'X XXXX XXX XXXXXXXXXX XXX XXXX X',
+    'X               E              X',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+    x = y = 0
+    for row in level:
+        for col in row:
+            if col == "X":
+                Wall((x, y))
+            if col == "E":
+                end_rect = pygame.Rect(x, y, b_p, b_p)
+            x += b_p
+        y += b_p
+        x = 0
+
+def init():
+    global screen,size, horizontal, vertical, b_p, width_mid
+    pygame.init()
+    b_p = 20
+    size = horizontal , vertical= 64*b_p, 33*b_p     
+    screen = pygame.display.set_mode((size))
     width_mid = 3//2
+    image_music_display()
+    colours()
+    colision()
 
-char_map ="""XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-X              XX              X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X XX        XX XX XX        XX X
-X XX XXXXXX XX XX XX XXXXXX XX X
-X    XXXXXX          XXXXXX    X
-X XX XX     XXXXXXXX     XX XX X
-X XX XXXXXXXXXXXXXXXXXXXXXX XX X
-X XX XXXXXXXXXXXXXXXXXXXXXX XX X
-X              XX              X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X      XXX            XXX      X
-XXXXXX XXX XXXX  XXXX XXX XXXXXX
-XXXXXX XXX X        X XXX XXXXXX
-           X        X           
-XXXXXX XXX X        X XXX XXXXXX
-XXXXXX XXX XXXXXXXXXX XXX XXXXXX
-X      XXX            XXX      X
-X XXXX XXX XXXXXXXXXX XXX XXXX X
-X XXXX XXX XXXXXXXXXX XXX XXXX X
-X XXXX         XX              X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X XXXX XXXXXXX XX XXXXXXX XXXX X
-X          XXX    XXX          X
-X XXXXXXXX XXX XX XXX XXXXXXXX X
-X XXXXXXXX XXX XX XXX XXXXXXXX X
-X      XXX     XX     XXX      X
-X XXXX XXX XXXXXXXXXX XXX XXXX X
-X XXXX XXX XXXXXXXXXX XXX XXXX X
-X                              X
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"""
-
-def tiles(mapa):
-    global tile
-    for y, line in enumerate(mapa):
-        for x, c in enumerate(line):
-            if c =="X":
-                screen.blit(tile,(x*16,y*16))
-
-def buttons(b_x, b_y, b_with, b_height, b_color, text, surface,color_font):
+def buttons(b_x, b_y, b_with, b_height, text, surface):
 
     button = pygame.Rect(int(b_x), int(b_y), int(b_with), int(b_height))
-    pygame.draw.rect(surface, b_color, button) #b_color (40, 95, 141)
+    pygame.draw.rect(surface, (40, 95, 141), button)
             
     b_font = pygame.font.Font('data/game_over.ttf', b_with//3)
 
-    b_text = b_font.render(text,True,color_font)
+    b_text = b_font.render(text,True,(255,255,255))
     surface.blit(b_text,(int(b_x) + b_with//2 - b_text.get_rect().width//2, int(b_y) + b_height//2 - b_text.get_rect().height//2))
     
     return  button
 
+def entity_collicion():
+    if player.rect.colliderect(end_rect):
+        pygame.quit()
+        sys.exit()
+
+def get_pressed(key):
+    if key[pygame.K_w]:
+        player.move( 0,-2)
+    if key[pygame.K_s]:
+        player.move( 0, 2)
+    if key[pygame.K_a]:
+        player.move(-2, 0)
+    if key[pygame.K_d]:
+        player.move( 2, 0)
+
+def buttons_menu():
+    fuente = pygame.font.Font('data/game_over.ttf', 300)
+    Title_G = fuente.render("Pacman Invader", True, (255,255,255))
+    screen.blit(Title_G, (horizontal//2 - Title_G.get_rect().width//2, 10))
+    pos_x = horizontal/2 - 150
+    pos_y = Title_G.get_rect().height + 30
+    rest =  vertical - pos_y
+    b_start = buttons(pos_x,pos_y + rest-(rest/5*5),300,50,"Start",screen)
+    b_options = buttons(pos_x,pos_y + rest-(rest/5*4),300,50,"Options",screen)
+    b_shop = buttons(pos_x,pos_y + rest-(rest/5*3),300,50,"Shop",screen)
+    b_credits = buttons(pos_x,pos_y + rest-(rest/5*2),300,50,"Credits",screen)
+    b_exit = buttons(pos_x,pos_y + rest-(rest/5*1),300,50,"Exit",screen)
+    x, y = pygame.mouse.get_pos()
+    if b_start.collidepoint(x,y):
+        if pygame.mouse.get_pressed()[0] == 1:
+            game()
+    if b_options.collidepoint(x,y):
+        if pygame.mouse.get_pressed()[0] == 1:
+            Options()
+    if b_shop.collidepoint(x,y):
+        if pygame.mouse.get_pressed()[0] == 1:
+            Shop()
+    if b_credits.collidepoint(x,y):
+        if pygame.mouse.get_pressed()[0] == 1:
+            Credits()
+    if b_exit.collidepoint(x,y):
+        if pygame.mouse.get_pressed()[0] == 1:
+            on = Exit()
+
+def col_bg_process():
+    screen.fill((0, 0, 0))
+    pygame.mouse.set_visible(True)
+def pacman_process():
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+    pygame.draw.rect(screen, (255, 0, 0), end_rect)
+    pygame.draw.rect(screen, (255, 200, 0), player.rect)
+
 def main_menu():
-    global mapa
-    mapa = char_map.splitlines()
     init()
     on = True
     while on:
-        screen.fill((0,0,0))
-        pygame.mouse.set_visible(True)
-
-        fuente = pygame.font.Font('data/game_over.ttf', 300)
-        Title_G = fuente.render("Pacman Invader", True, (255,255,255))
-        screen.blit(Title_G, (horizontal//2 - Title_G.get_rect().width//2, 10))
-
-        pos_x = horizontal/2 - 150
-        pos_y = Title_G.get_rect().height + 30
-        rest =  vertical - pos_y
-        x, y = pygame.mouse.get_pos()
-
-        if pos_x + 300 > x > pos_x and pos_y + rest-(rest/5*5) + 50 > y > pos_y + rest-(rest/5*5):
-            b_start = buttons(pos_x,pos_y + rest-(rest/5*5), 300, 50, colour_b1, "Start", screen, white)
-        else:
-            b_start = buttons(pos_x,pos_y + rest-(rest/5*5), 300, 50, white, "Start", screen, black)
-        
-        if pos_x + 300 > x > pos_x and pos_y + rest-(rest/5*4) + 50 > y > pos_y + rest-(rest/5*4):
-           b_options = buttons(pos_x,pos_y + rest-(rest/5*4), 300, 50, colour_b1, "Options", screen, white)
-        else:
-            b_options = buttons(pos_x,pos_y + rest-(rest/5*4), 300, 50, white, "Options", screen, black)
-        
-        if pos_x + 300 > x > pos_x and pos_y + rest-(rest/5*3) + 50 > y > pos_y + rest-(rest/5*3):
-            b_shop = buttons(pos_x,pos_y + rest-(rest/5*3), 300, 50, colour_b1, "Shop", screen, white)
-        else:
-            b_shop = buttons(pos_x,pos_y + rest-(rest/5*3), 300, 50, white, "Shop", screen, black)
-
-        if pos_x + 300 > x > pos_x and pos_y + rest-(rest/5*2) + 50 > y > pos_y + rest-(rest/5*2):
-            b_credits = buttons(pos_x, pos_y + rest-(rest/5*2), 300, 50, colour_b1, "Credits", screen, white)
-        else:
-            b_credits = buttons(pos_x, pos_y + rest-(rest/5*2), 300, 50, white, "Credits", screen, black)
-        
-        if pos_x + 300 > x > pos_x and pos_y + rest-(rest/5*1) + 50 > y > pos_y + rest-(rest/5*1):
-            b_exit = buttons(pos_x, pos_y + rest-(rest/5*1), 300, 50, colour_b1, "Exit", screen, white)
-        else:
-            b_exit = buttons(pos_x, pos_y + rest-(rest/5*1), 300, 50, white, "Exit", screen, black)
-               
-        
-        if b_start.collidepoint(x,y):
-            if pygame.mouse.get_pressed()[0] == 1:
-                game()
-        if b_options.collidepoint(x,y):
-            if pygame.mouse.get_pressed()[0] == 1:
-                Options()
-        if b_shop.collidepoint(x,y):
-            if pygame.mouse.get_pressed()[0] == 1:
-                Shop()
-        if b_credits.collidepoint(x,y):
-            if pygame.mouse.get_pressed()[0] == 1:
-                Credits()
-        if b_exit.collidepoint(x,y):
-            if pygame.mouse.get_pressed()[0] == 1:
-                on = Exit()
-                
+        col_bg_process()
+        buttons_menu()
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 on = False
@@ -192,9 +236,13 @@ def game():
 
     on = True
     while on:
-        screen.fill(background)
-        tiles(mapa)
+        screen.fill(col_bg)
         pygame.draw.line(screen,white,[(horizontal//2),0],[(horizontal//2),vertical],width_mid)
+        pacman_process()
+        entity_collicion()
+        clock.tick(360)                
+        key = pygame.key.get_pressed()
+        get_pressed(key) # qué tecla se está aprentando?
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 on = False
@@ -372,4 +420,5 @@ def menu_title(title):
     fuente = pygame.font.Font('data/game_over.ttf', 300)
     Title_G = fuente.render(title, True, (255,255,255))
     screen.blit(Title_G, (horizontal//2 - Title_G.get_rect().width//2, 50))
+
 main_menu()
